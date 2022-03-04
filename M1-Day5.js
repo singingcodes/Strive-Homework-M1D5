1d2e9c0038af8e49a1d4b2b62bffdd9ad7eaa174
Sarah Ayanwale <sarahayanwale@gmail.com>
2022-03-04 16:43:39 +0100

    solved Questions 1-8

---
+++ M1-Day5.js
@@ -0,0 +1,127 @@
+/*
+ASSIGNMENT RULES
+- All the answers must be written in JavaScript
+- You can ask for help, reach the Teaching Assistants if needed
+- You can Google / use StackOverflow BUT only when you think you need something we didn't cover during lessons yet
+- You can test your code in a separate file or de-commenting the single exercises in this one.
+- You can use the bash terminal, the VSCode terminal or the one embedded in your Operating System if you're using macOS or Linux.
+- The solution must be pushed to the repository and be available for the tutors by the end of the day (5PM CET)
+*/
+
+/* EXERCISE 1
+ Write a function called "area" which receives 2 parameters (l1, l2) and calculates the area of the associated rectangle.
+*/
+
+function area(l1, l2) {
+  return l1 * l2;
+}
+
+/* EXERCISE 2
+ Write a function called "crazySum" which receives two integers as parameters.
+ It should return the sum of those two values, but if the two values are the same then it should return their sum multiplied by 3.
+*/
+
+function crazySum(integer1, integer2) {
+  let sum = integer1 + integer2;
+  if (integer1 === integer2) {
+    return sum * 3;
+  } else {
+    return sum;
+  }
+}
+
+/* EXERCISE 3
+ Write a function called "crazyDiff" that computes the absolute difference between a given number and 19.
+ It should return triple their absolute difference if the given number is greater than 19.
+*/
+
+let crazyDiff = (number1) => {
+  let difference = number1 - 19;
+  if (number1 > 19) {
+    return difference * 3;
+  } else {
+    return difference;
+  }
+};
+
+/* EXERCISE 4
+ Write a function called "boundary" which accept an integer parameter n and returns true if n is within 20 and 100 (included) or if n it's equal to 400.
+*/
+let n = 3;
+function boundary(n) {
+  if (n <= 20 || n <= 100 || n === 400) {
+    return true;
+  } else return false;
+}
+
+/* EXERCISE 5
+ Write a function called "strivify" which accepts a string as a parameter.
+ It should add the word "Strive" in front of the given string, but if the given string already begins with "Strive", then it should just return the original string.
+*/
+function strivify(param1) {
+  let striveParam = "";
+  if (striveParam.startsWith("Strive", 0)) {
+    console.log(param1);
+  } else {
+    console.log("Strive " + param1);
+  }
+}
+let strive = strivify("Sarah");
+console.log(strive);
+/* EXERCISE 6
+ Write a function called "check3and7" which accepts a positive number as a parameter and checks if it is a multiple of 3 or a multiple of 7.
+ HINT: Modulus Operator
+*/
+
+function check3and7(y) {
+  if (y % 3 === 0 || y % 7 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+let answer = check3and7(21);
+
+//console.log(answer);
+
+/* EXERCISE 7
+ Write a function called "reverseString" which programmatically reverses a given string (es.: Strive => evirtS).
+*/
+function reverseString(str) {
+  let newString = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    newString += str[i];
+  }
+  return newString;
+}
+let reverse = reverseString("strive");
+console.log(reverse);
+/* EXERCISE 8
+ Write a function called "upperFirst" which capitalizes the first letter of each word of a given string passed as a parameter.
+*/
+
+function upperFirst(words) {
+  var separateWord = words.toLowerCase().split(" ");
+  for (var i = 0; i < separateWord.length; i++) {
+    separateWord[i] =
+      separateWord[i].charAt(0).toUpperCase() + separateWord[i].substring(1);
+  }
+  return separateWord.join(" ");
+}
+console.log(upperFirst("This is strive school"));
+
+/* EXERCISE 9
+ Write a function called "cutString" which creates a new string without the first and last character of a given string passed as a parameter.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* EXERCISE 10
+ Write a function called "giveMeRandom" which accepts a number n and returns an array containing n random numbers between 0 and 10.
+*/
+
+/* WRITE YOUR ANSWER HERE */
+
+/* WHEN YOU ARE FINISHED
+ Commit and push the code to your personal GitHub repository; then post the link of your commit on the Homework section of today's Eduflow.
+*/
